e6eed41fdfef97a5f55cdaf16cf20df9a59c2fbf
Rajdeep Kaur <rajdeep51994@gmail.com>
2016-02-27 16:07:07 +0530

    qsTr only for literals in js file no more in qml of family

--- src/activities/family/family.js
+++ src/activities/family/family.js
@@ -34,107 +34,107 @@ content of answer button and then 7th element refer to the correct answer then n
 other people, the contents that will be displayed next to bambo at last we will mention a flag that is either image or text
 */
 
-var dataset = [  [ "man1.svg","girl1.svg","grandmother.svg","Grand Mother","Grand Father","Father","Grand Mother",
-                  "?","Father","Mother","image",3],
+var dataset = [  [ "man1.svg","girl1.svg","grandmother.svg",qsTr("Grand Mother"),qsTr("Grand Father"),qsTr("Father"),"Grand Mother",
+                  qsTr("?"),qsTr("Father"),qsTr("Mother"),"image",3],
 
-               [ "grandpa.svg","girl2.svg","man1.svg","Grand Mother", "Mother", "Grand Father","Grand Father",
-                "Father","?","Son","image",3],
+               [ "grandpa.svg","girl2.svg","man1.svg",qsTr("Grand Mother"),qsTr("Mother"), qsTr("Grand Father"),"Grand Father",
+                qsTr("Father"),qsTr("?"),qsTr("Son"),"image",3],
 
-               [ "girl3.svg","man1.svg","man2.svg","Uncle","Aunty","father","Uncle","Brother","Daughter","?",
+               [ "girl3.svg","man1.svg","man2.svg",qsTr("Uncle"),qsTr("Aunty"),qsTr("father"),"Uncle",qsTr("Brother"),qsTr("Daughter"),qsTr("?"),
                 "image",3],
 
-               [ "lady.svg","boy1.svg","lady1.svg","Mother","Aunty","GrandMother","Aunty","Mother","?","Sister",
+               [ "lady.svg","boy1.svg","lady1.svg",qsTr("Mother"),qsTr("Aunty"),qsTr("GrandMother"),"Aunty",qsTr("Mother"),qsTr("?"),qsTr("Sister"),
                 "image",3],
 
-               [ "lady.svg","boy1.svg","girl2.svg","Brother","Sister","Cousin","Cousin","?","Mother","Niece",
+               [ "lady.svg","boy1.svg","girl2.svg",qsTr("Brother"),qsTr("Sister"),qsTr("Cousin"),"Cousin",qsTr("?"),qsTr("Mother"),qsTr("Niece"),
                 "image",3],
 
-               [ "man1.svg","man2.svg","boy1.svg","Daughter","Son","Newphew","Newphew","Son","Brother","?",
+               [ "man1.svg","man2.svg","boy1.svg",qsTr("Daughter"),qsTr("Son"),qsTr("Newphew"),"Newphew",qsTr("Son"),qsTr("Brother"),qsTr("?"),
                 "image",3],
 
-               [ "girl3.svg","man1.svg","man2.svg","Newphew","Niece","Daughter","Niece","Brother","?","Father",
+               [ "girl3.svg","man1.svg","man2.svg",qsTr("Newphew"),qsTr("Niece"),qsTr("Daughter"),"Niece",qsTr("Brother"),qsTr("?"),qsTr("Father"),
                 "image",3],
 
-               [ "grandmother.svg","man1.svg","boy1.svg","Grand Son", "Son","Son In Law","Grand Son",
-                "Son","Son","?","image",3],
+               [ "grandmother.svg","man1.svg","boy1.svg",qsTr("Grand Son"),qsTr("Son"),qsTr("Son In Law"),"Grand Son",
+                qsTr("Son"),qsTr("Son"),qsTr("?"),"image",3],
 
-               [ "lady.svg","girl1.svg","grandpa.svg","Grand Daughter","Daughter","Daughter In Law","Grand Daughter",
-                "?","Mother","Father","image",3] ,
+               [ "lady.svg","girl1.svg","grandpa.svg",qsTr("Grand Daughter"),qsTr("Daughter"),qsTr("Daughter In Law"),"Grand Daughter",
+                qsTr("?"),qsTr("Mother"),qsTr("Father"),"image",3] ,
 
-               [ "man1.svg","lady1.svg","grandmother.svg","Mother","Mother in law","Aunty","Mother in law",
-                "Mother","Husband","?","image",3],
+               [ "man1.svg","lady1.svg","grandmother.svg",qsTr("Mother"),qsTr("Mother in law"),qsTr("Aunty"),"Mother in law",
+                qsTr("Mother"),qsTr("Husband"),qsTr("?"),"image",3],
 
-               [ "girl2.svg","man2.svg","grandpa.svg","Father", "Father in Law", "Uncle","Father in Law",
-                "Father","Wife","?","image",3],
+               [ "girl2.svg","man2.svg","grandpa.svg",qsTr("Father"),qsTr("Father in Law"),qsTr("Uncle"),"Father in Law",
+                qsTr("Father"),qsTr("Wife"),qsTr("?"),"image",3],
 
-               [ "girl3.svg","man1.svg","girl2.svg","Sister","Cousin","Sister in Law","Sister in Law",
-                "?","Wife","Sister","image",3],
+               [ "girl3.svg","man1.svg","girl2.svg",qsTr("Sister"),qsTr("Cousin"),qsTr("Sister in Law"),"Sister in Law",
+                qsTr("?"),qsTr("Wife"),qsTr("Sister"),"image",3],
 
-               [ "man2.svg","girl3.svg","man1.svg","Brother","Brother in law","Cousin","Brother in law",
-                "?","Husband","Brother","image",3],
+               [ "man2.svg","girl3.svg","man1.svg",qsTr("Brother"),qsTr("Brother in law"),qsTr("Cousin"),"Brother in law",
+                qsTr("?"),qsTr("Husband"),qsTr("Brother"),"image",3],
 
-               [ "man1.svg","boy1.svg","lady1.svg","Mother","Aunty","Step-Mother","Step-Mother",
-                "?","Father","Second Wife","image",3],
+               [ "man1.svg","boy1.svg","lady1.svg",qsTr("Mother"),qsTr("Aunty"),qsTr("Step-Mother"),"Step-Mother",
+                qsTr("?"),qsTr("Father"),qsTr("Second Wife"),"image",3],
 
-               [ "lady.svg","girl2.svg","man2.svg","Father","Uncle","Step-Father","Step-Father",
-                "?","Mother","Second Husband","image",3],
+               [ "lady.svg","girl2.svg","man2.svg",qsTr("Father"),qsTr("Uncle"),qsTr("Step-Father"),"Step-Father",
+                qsTr("?"),qsTr("Mother"),qsTr("Second Husband"),"image",3],
 
-               [ "boy2.svg","man1.svg","lady.svg","Son","Step-Son","Newphew","Step-Son",
-                "Second Wife","?","Mother","image",3],
+               [ "boy2.svg","man1.svg","lady.svg",qsTr("Son"),qsTr("Step-Son"),qsTr("Newphew"),"Step-Son",
+               qsTr("Second Wife"),qsTr("?"),qsTr("Mother"),"image",3],
 
-               [ "lady1.svg","man2.svg","girl2.svg","Step-Daughter","Niece","Daughter","Step-Daughter",
-                "Daughter","Second wife","?","image",3],
+               [ "lady1.svg","man2.svg","girl2.svg",qsTr("Step-Daughter"),qsTr("Niece"),qsTr("Daughter"),"Step-Daughter",
+                qsTr("Daughter"),qsTr("Second wife"),qsTr("?"),"image",3],
 
-               [ "Father","You","Mother","Grand Mother","Grand Father","Father","Grand Mother",
-                "?","","","text",3],
+               [ qsTr("Father"),qsTr("You"),qsTr("Mother"),qsTr("Grand Mother"),qsTr("Grand Father"),qsTr("Father"),"Grand Mother",
+                qsTr("?"),qsTr(""),qsTr(""),"text",3],
 
-               [ "Father","You","Father","Grand Mother","Mother", "Grand Father","Grand Father",
-                "?","","","text",3],
+               [ qsTr("Father"),qsTr("You"),qsTr("Father"),qsTr("Grand Mother"),qsTr("Mother"),qsTr("Grand Father"),"Grand Father",
+                qsTr("?"),qsTr(""),qsTr(""),"text",3],
 
-               [ "You","Son","Brother","Uncle","Aunty","father","Uncle","","","?","text",3],
+               [qsTr("You"),qsTr("Son"),qsTr("Brother"),qsTr("Uncle"),qsTr("Aunty"),qsTr("father"),"Uncle",qsTr(""),qsTr(""),qsTr("?"),"text",3],
 
-               [ "You","Daughter","Sister","Mother","Aunty","GrandMother","Aunty","","","?",
+               [ qsTr("You"),qsTr("Daughter"),qsTr("Sister"),qsTr("Mother"),qsTr("Aunty"),qsTr("GrandMother"),"Aunty",qsTr(""),qsTr(""),qsTr("?"),
                 "text",3],
 
-               [ "Father","You","Newphew","Brother","Sister","Cousin","Cousin","?","","",
+               [ qsTr("Father"),qsTr("You"),qsTr("Newphew"),qsTr("Brother"),qsTr("Sister"),qsTr("Cousin"),"Cousin",qsTr("?"),qsTr(""),qsTr(""),
                 "text",3],
 
-               [ "Father","You","Niece","Brother","Sister","Cousin","Cousin","?","","",
+               [ qsTr("Father"),qsTr("You"),qsTr("Niece"),qsTr("Brother"),qsTr("Sister"),qsTr("Cousin"),"Cousin",qsTr("?"),qsTr(""),qsTr(""),
                 "text",3],
 
-               ["Father","You","Cousin","Daughter","Son","Newphew","Newphew","","","?",
+               [qsTr("Father"),qsTr("You"),qsTr("Cousin"),qsTr("Daughter"),qsTr("Son"),qsTr("Newphew"),"Newphew",qsTr(""),qsTr(""),qsTr("?"),
                 "text",3],
 
-               ["Mother","You","Cousin","Son","Niece","Daughter","Niece","","","?",
+               [qsTr("Mother"),qsTr("You"),qsTr("Cousin"),qsTr("Son"),qsTr("Niece"),qsTr("Daughter"),"Niece",qsTr(""),qsTr(""),qsTr("?"),
                 "text",3],
 
-               ["Son","You","Son","Grand Son", "Son","Son In Law","Grand Son","?","","","text",3],
+               [qsTr("Son"),qsTr("You"),qsTr("Son"),qsTr("Grand Son"),qsTr("Son"),qsTr("Son In Law"),"Grand Son",qsTr("?"),qsTr(""),qsTr(""),"text",3],
 
-               ["Son","You","Daughter","Grand Daughter","Daughter","Daughter In Law","Grand Daughter",
-                "?","","","text",3],
+               [qsTr("Son"),qsTr("You"),qsTr("Daughter"),qsTr("Grand Daughter"),qsTr("Daughter"),qsTr("Daughter In Law"),"Grand Daughter",
+                qsTr("?"),qsTr(""),qsTr(""),"text",3],
 
-               ["Wife","You","Mother","Mother","Mother in law","Aunty","Mother in law",
-                "?","","","text",3],
+               [qsTr("Wife"),qsTr("You"),qsTr("Mother"),qsTr("Mother"),qsTr("Mother in law"),qsTr("Aunty"),"Mother in law",
+                qsTr("?"),qsTr(""),qsTr(""),"text",3],
 
-               ["husband","You","Father","Father", "Father in Law", "Uncle","Father in Law",
-                "?","","","text",3],
+               [qsTr("husband"),qsTr("You"),qsTr("Father"),qsTr("Father"),qsTr("Father in Law"),qsTr("Uncle"),"Father in Law",
+                qsTr("?"),qsTr(""),qsTr(""),"text",3],
 
-               ["Wife","You","Brother","Brother","Brother in law","Cousin","Brother in law",
-                "?","","","text",3],
+               [qsTr("Wife"),qsTr("You"),qsTr("Brother"),qsTr("Brother"),qsTr("Brother in law"),qsTr("Cousin"),"Brother in law",
+                qsTr("?"),qsTr(""),qsTr(""),"text",3],
 
-               ["Wife","You","Sister","Sister","Cousin","Sister in Law","Sister in Law",
-                "?","","","text",3],
+               [qsTr("Wife"),qsTr("You"),qsTr("Sister"),qsTr("Sister"),qsTr("Cousin"),qsTr("Sister in Law"),"Sister in Law",
+                qsTr("?"),qsTr(""),qsTr(""),"text",3],
 
-               ["Father","Second\nWife","You", "Mother","Aunty","Step-Mother","Step-Mother",
-                "?","","","text",3],
+               [qsTr("Father"),qsTr("Second\nWife"),qsTr("You"),qsTr("Mother"),qsTr("Aunty"),qsTr("Step-Mother"),qsTr("Step-Mother"),
+                qsTr("?"),qsTr(""),qsTr(""),"text",3],
 
-               ["Mother","Second\nHusband","You","Father","Uncle","Step-Father","Step-Father",
-                "?","","","text",3],
+               [qsTr("Mother"),qsTr("Second\nHusband"),qsTr("You"),qsTr("Father"),qsTr("Uncle"),qsTr("Step-Father"),"Step-Father",
+                qsTr("?"),qsTr(""),qsTr(""),"text",3],
 
-               ["Second\nWife","You","Son","Son","Step-Son","Newphew","Step-Son","?","","","text",3],
+               [qsTr("Second\nWife"),qsTr("You"),qsTr("Son"),qsTr("Son"),qsTr("Step-Son"),qsTr("Newphew"),"Step-Son",qsTr("?"),qsTr(""),qsTr(""),"text",3],
 
-               ["Second\nWife","You","Daughter","Step-Daughter","Niece","Daughter","Step-Daughter",
-                "?","","","text",3]
+               [qsTr("Second\nWife"),qsTr("You"),qsTr("Daughter"),qsTr("Step-Daughter"),qsTr("Niece"),qsTr("Daughter"),qsTr("Step-Daughter"),
+                qsTr("?"),qsTr(""),qsTr(""),"text",3]
     ];
 
 function start(items_) {
@@ -153,17 +153,17 @@ function initLevel() {
     currentdata = dataset[items.bar.level];
     items.imageOrtext = currentdata[10];
     count = currentdata[11];
-    items.repeateanswerbutton.model = [qsTr(currentdata[3]),qsTr(currentdata[4]),qsTr(currentdata[5])]
+    items.repeateanswerbutton.model = [currentdata[3],currentdata[4],currentdata[5]]
     if(currentdata[10] === "text") {
             items.pic1.source = "qrc:/gcompris/src/activities/family/resource/" + "text_background.svg"
             items.pic1.visible = false
-            items.text1.text = qsTr(currentdata[0])
+            items.text1.text = currentdata[0]
             items.pic2.source = "qrc:/gcompris/src/activities/family/resource/" + "text_background.svg"
             items.pic2.visible = false
-            items.text2.text = qsTr(currentdata[1])
+            items.text2.text = currentdata[1]
             items.pic3.source = "qrc:/gcompris/src/activities/family/resource/" + "text_background.svg"
             items.pic3.visible = false
-            items.text3.text = qsTr(currentdata[2])
+            items.text3.text = currentdata[2]
 
     }  else {
         items.pic1.source = "qrc:/gcompris/src/activities/family/resource/" + currentdata[0]
@@ -174,7 +174,7 @@ function initLevel() {
         items.pic3.visible = true
     }
     items.answered = currentdata[6];
-    items.caption1d = qsTr(currentdata[7]); items.caption2d = qsTr(currentdata[8]); items.caption3d = qsTr(currentdata[9]);
+    items.caption1d = currentdata[7]; items.caption2d =currentdata[8]; items.caption3d = currentdata[9];
 }
 
 function nextLevel() {
